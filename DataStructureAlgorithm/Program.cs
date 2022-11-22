d71d7e445f7b80a73f763cef0e9e8b02ac45c313
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-22 12:22:24 +0530

    (Shubham)Add: Project folder structure created

---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace DataStructureAlgorithm;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Algorithm Programs");
+    }
+}
