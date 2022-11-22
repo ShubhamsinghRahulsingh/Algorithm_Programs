a1dbbfc099171341e4515ea02dcd2a15861a30de
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-22 16:13:09 +0530

    (Shubham)Add: Sorting integer array using Bubble sort

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -5,12 +5,13 @@ class Program
     public static string binaryTextPath = @"D:\GitRepository\Algorithm_Programs\DataStructureAlgorithm\Files\BinarySearch.txt";
     public static void Main(string[] args)
     {
+        int[] array = { 5, 1, 8, 7, 2, 4, 6, 9, 3, 10 };
         Console.WriteLine("Welcome to the Algorithm Programs");
         bool flag = true;
         while(flag)
         {
             Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("1.BinarySearch\n2.InsertionSort\n3.Exit");
+            Console.WriteLine("1.BinarySearch\n2.InsertionSort\n3.BubbleSort\n4.Exit");
             Console.Write("Select any one from the above option: ");
             int choice=Convert.ToInt32(Console.ReadLine());
             switch(choice)
@@ -26,6 +27,12 @@ class Program
                     sort.Sort();
                     break;
                 case 3:
+                    BubbleSort bubblesort = new BubbleSort();
+                    Console.WriteLine("Before sorting: ");
+                    bubblesort.Display(array);
+                    bubblesort.Sort(array);
+                    break;
+                case 4:
                     flag = false;
                     break;
             }
