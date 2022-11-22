3e24f9bf8b54b15473b4c4bb4ef4c73c0dba5af8
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-22 15:31:37 +0530

    (Shubham)Add: Search the word from word list using Binary search

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -2,8 +2,28 @@
 namespace DataStructureAlgorithm;
 class Program
 {
+    public static string binaryTextPath = @"D:\GitRepository\Algorithm_Programs\DataStructureAlgorithm\Files\BinarySearch.txt";
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Algorithm Programs");
+        bool flag = true;
+        while(flag)
+        {
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("1.BinarySearch\n2.Exit");
+            Console.Write("Select any one from the above option: ");
+            int choice=Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    BinarySearch search=new BinarySearch();
+                    search.ReadFile(binaryTextPath);
+                    search.BinarySearchWord("bridgelabz");
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
